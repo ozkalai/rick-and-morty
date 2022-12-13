39d5929cf6aee4ee3c810684922c697970109213
Melih Özkalay <melihozkalayy@gmail.com>
2022-12-13 23:35:43 +0300

    feat: add redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
-import locationsReducer from "./slices/locations";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import locationReducer from "./slices/locations";
+import characterReducer from "./slices/characters";
+
+const rootReducer = combineReducers({
+  location: locationReducer,
+  character: characterReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    locations: locationsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
