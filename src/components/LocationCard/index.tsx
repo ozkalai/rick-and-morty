ef609762c47eaefd3476fcaca699a4aafb1b7e7c
Melih Özkalay <melihozkalayy@gmail.com>
2022-12-14 13:58:17 +0300

    feat: update location card

--- src/components/LocationCard/index.tsx
+++ src/components/LocationCard/index.tsx
@@ -3,6 +3,7 @@ import styles from "./LocationCard.module.scss";
 
 const LocationCard = ({ location }: { location: Location }) => {
   const { name, type, dimension, residents } = location;
+
   return (
     <div className={styles.card}>
       <h2 className={styles.card__header}>{name}</h2>
@@ -10,7 +11,8 @@ const LocationCard = ({ location }: { location: Location }) => {
         <div className={styles.card__subheader}>Type</div>: {type}
       </div>
       <div className={styles.card__info}>
-        <div className={styles.card__subheader}>Dimension</div>: {dimension}
+        <div className={styles.card__subheader}>Dimension</div>:{" "}
+        {dimension === "unknown" ? "-" : dimension}
       </div>
       <div className={styles.card__info}>
         <div className={styles.card__subheader}>Residents count</div>:{" "}
