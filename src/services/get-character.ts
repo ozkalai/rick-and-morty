140d5ed90f64f6796c195b03b959d1a2665ab1ce
Melih Özkalay <melihozkalayy@gmail.com>
2022-12-15 01:28:40 +0300

    chore: update service with headers

--- src/services/get-character.ts
+++ src/services/get-character.ts
@@ -4,7 +4,12 @@ import { Character } from "../types/Character";
 export const getCharacter = async (id: string) => {
   try {
     const response: Character = await fetch(
-      `https://rickandmortyapi.com/api/character/${id}`
+      `https://rickandmortyapi.com/api/character/${id}`,
+      {
+        headers: {
+          accept: "application/json",
+        },
+      }
     ).then((res) => res.json());
     return response;
   } catch (error) {
