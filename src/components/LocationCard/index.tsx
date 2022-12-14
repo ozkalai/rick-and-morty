5d22780abef3ad29e0597078d7c6b08d5997eba3
Melih Özkalay <melihozkalayy@gmail.com>
2022-12-14 22:06:51 +0300

    feat: update location card with onclick prop

--- src/components/LocationCard/index.tsx
+++ src/components/LocationCard/index.tsx
@@ -1,11 +1,17 @@
 import { Location } from "../../types/Location";
 import styles from "./LocationCard.module.scss";
 
-const LocationCard = ({ location }: { location: Location }) => {
+const LocationCard = ({
+  location,
+  onClick,
+}: {
+  location: Location;
+  onClick?: () => void;
+}) => {
   const { name, type, dimension, residents } = location;
 
   return (
-    <div className={styles.card}>
+    <div onClick={onClick} className={styles.card}>
       <h2 className={styles.card__header}>{name}</h2>
       <div className={styles.card__info}>
         <div className={styles.card__subheader}>Type</div>: {type}
